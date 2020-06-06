8431f7fa80c62438edd3810bcd50771485f6941c
Kevin Pfeifer <kevin.pfeifer@kvnp.eu>
2020-06-06 17:40:17 +0200

    display youtube musics homepage

--- tabs/HomeTab.js
+++ tabs/HomeTab.js
@@ -4,140 +4,63 @@ import {
     View,
     Dimensions,
     Text,
-    StyleSheet
+    StyleSheet,
+    ScrollView,
+    TouchableHighlight
 } from 'react-native';
 
 import {
     Header
-} from '../components/SearchComponents.js';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { ScrollView } from 'react-native-gesture-handler';
+} from '../components/SharedComponents'
 
-export default class SearchTab extends Component {
+import {
+    Results
+} from '../components/HomeComponents';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import * as Tube from '../modules/Tube';
+
+export default class HomeTab extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            results: null
+            home: null,
+            background: require("../assets/img/header.jpg"),
         };
     }
 
-    resultReceiver = (childData) => {
-        this.setState({results: childData});
-    };
+    onPressRefresh = () => {
+        Tube.fetchHome().then((result) => {
+
+            /*let source = require("../assets/img/header.jpg");
+            if (result.background != undefined) source = {uri: result.background};
+            
+            this.setState({home: result, background: source});*/
+
+            if (result.background != undefined) this.setState({home: result, background: result.background});
+            else                                this.setState({home: result, background: "../assets/img/header.jpg"});
+        });
+        
+        
+    }
 
     render() {
         return (
             <>
                 <View style={styles.headerPicture}>
-                    <Header style={{borderRadius: 100}} text={"Home"}/>
+                    <Header style={{borderRadius: 100}} sourcee={this.state.background}  text={"Home"}/>
                 </View>
 
                 <View style={styles.middleView}>
                     <ScrollView style={styles.homeView}>
-                        <Text style={styles.homeText}>New releases</Text>
-                        <ScrollView style={styles.albumCollection} horizontal={true}>
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>New Release Mix</Text>
-                                <Text style={styles.albumDesc}>Just updated!</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Shaniwar Special Bhajans</Text>
-                                <Text style={styles.albumDesc}>Single • Sonu Nigam {"&"} Poonam Lakkha</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>GOOBA</Text>
-                                <Text style={styles.albumDesc}>Single • 6ix9ine</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Jeetenge Hum</Text>
-                                <Text style={styles.albumDesc}>Single • Dhvani Bhanushali</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Chromatica</Text>
-                                <Text style={styles.albumDesc}>Album • Lady Gaga</Text>
-                            </View>
-                        </ScrollView>
-
-                        <Text style={styles.homeText}>Morning Preparation</Text>
-                        <ScrollView style={styles.albumCollection} horizontal={true}>
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Boolywood Coolers</Text>
-                                <Text style={styles.albumDesc}>Playlist • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Bollywood Happy Songs</Text>
-                                <Text style={styles.albumDesc}>Playlist • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Hindi Mood Elevating Mixtape</Text>
-                                <Text style={styles.albumDesc}>Playlist • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Bollywood Morning Beats</Text>
-                                <Text style={styles.albumDesc}>Playlist • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Life is Good: Bollywood</Text>
-                                <Text style={styles.albumDesc}>Playlist • YouTube Music</Text>
-                            </View>
-                        </ScrollView>
-
-                        <Text style={styles.homeText}>Charts</Text>
-                        <ScrollView style={styles.albumCollection} horizontal={true}>
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Top 100 Songs India</Text>
-                                <Text style={styles.albumDesc}>Chart • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Top 100 Music Videos India</Text>
-                                <Text style={styles.albumDesc}>Chart • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Trending 20 India</Text>
-                                <Text style={styles.albumDesc}>Chart • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Top 100 Songs Global</Text>
-                                <Text style={styles.albumDesc}>Chart • YouTube Music</Text>
-                            </View>
-
-                            <View style={styles.album}>
-                                <View style={styles.albumCover}></View>
-                                <Text style={styles.albumTitle}>Top 100 Music Videos Global</Text>
-                                <Text style={styles.albumDesc}>Chart • YouTube Music</Text>
-                            </View>
-                        </ScrollView>
+                        <Results passthroughHome={this.state.home}/>
                     </ScrollView>
                 </View>
 
-                <View style={styles.refreshButton}>
-                    <Text>Aktualisieren</Text>
-                </View>
+                <TouchableHighlight onPress={this.onPressRefresh} style={styles.refreshButton}>
+                        <Text>Aktualisieren</Text>
+                </TouchableHighlight>
+                
             </>
         );
     }
