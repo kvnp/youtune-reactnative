70e84e4629f9f99417e112fd133747312b3f6e15
Kevin Pfeifer <kevin.pfeifer@kvnp.eu>
2020-06-13 04:37:56 +0200

    make header more readable

--- components/SharedComponents.js
+++ components/SharedComponents.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 import {
     ImageBackground,
-    Text
+    Text,
+    StyleSheet
 } from "react-native";
 
 export class Header extends Component {
@@ -17,11 +18,30 @@ export class Header extends Component {
     render() {
         return (
             <>
-                <ImageBackground imageStyle={{borderBottomLeftRadius:25, borderBottomRightRadius: 25}} style={{flex: 1, resizeMode: 'cover', alignItems: 'center', justifyContent: 'center'}}
+                <ImageBackground imageStyle={styles.imageStyle} style={styles.containerStyle}
                                  source={this.fetchImage(this.props.sourcee)} >
-                    <Text style={{color: this.props.color, fontSize: 45, fontWeight: 'bold'}}>{this.props.text}</Text>
+                    <Text style={[{color: this.props.color}, styles.textStyle]}>{this.props.text}</Text>
                 </ImageBackground>
             </>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    imageStyle: {
+        borderBottomLeftRadius:25,
+        borderBottomRightRadius: 25
+    },
+
+    containerStyle: {
+        flex: 1,
+        resizeMode: 'cover',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    textStyle: {
+        fontSize: 45,
+        fontWeight: 'bold'
+    },
+});
