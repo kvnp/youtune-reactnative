1f3b8efddef4cbedcc23a94ae3aed8312b9a2cbe
Kevin Pfeifer <kevin.pfeifer@kvnp.eu>
2020-06-13 04:41:34 +0200

    share current playlist/artist focus globally

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 
 global.apikey = null;
 
+global.playlist = {};
+global.artist = {};
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
