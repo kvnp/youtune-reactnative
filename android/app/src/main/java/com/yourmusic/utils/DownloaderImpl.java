442971b7e6639c78c81c5f9dbb92f399f6ad721b
Kevin Pfeifer <kevin.pfeifer@kvnp.eu>
2020-08-01 04:34:09 +0200

    replace okhttp3 with httpsurlconnection

--- android/app/src/main/java/com/yourmusic/utils/DownloaderImpl.java
+++ android/app/src/main/java/com/yourmusic/utils/DownloaderImpl.java
@@ -1,99 +1,102 @@
 package com.yourmusic.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import javax.net.ssl.HttpsURLConnection;
 
 import org.schabi.newpipe.extractor.downloader.Downloader;
 import org.schabi.newpipe.extractor.downloader.Request;
 import org.schabi.newpipe.extractor.downloader.Response;
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
 
-import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-
 public class DownloaderImpl extends Downloader {
-    private static final String USER_AGENT
-            = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/20100101 Firefox/68.0";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/20100101 Firefox/68.0";
     private static DownloaderImpl instance;
-    private OkHttpClient client;
 
-    private DownloaderImpl(final OkHttpClient.Builder builder) {
-        this.client = builder.readTimeout(30, TimeUnit.SECONDS).build();
-    }
+    private DownloaderImpl() {}
 
     /**
      * It's recommended to call exactly once in the entire lifetime of the application.
-     *
-     * @param builder if null, default builder will be used
      * @return a new instance of {@link DownloaderImpl}
      */
-    public static DownloaderImpl init(@Nullable final OkHttpClient.Builder builder) {
-        instance = new DownloaderImpl(
-                builder != null ? builder : new OkHttpClient.Builder());
-        return instance;
+    public static DownloaderImpl init() {
+        return new DownloaderImpl();
     }
 
     public static DownloaderImpl getInstance() {
         if (instance == null) {
-            init(null);
+            instance = init();
         }
         return instance;
     }
 
     @Override
-    public Response execute(@Nonnull final Request request)
-            throws IOException, ReCaptchaException {
+    public Response execute(@Nonnull final Request request) throws IOException, ReCaptchaException {
         final String httpMethod = request.httpMethod();
         final String url = request.url();
         final Map<String, List<String>> headers = request.headers();
         final byte[] dataToSend = request.dataToSend();
 
-        RequestBody requestBody = null;
-        if (dataToSend != null) {
-            requestBody = RequestBody.create(null, dataToSend);
-        }
+        final URL Url = new URL(url);
+        HttpsURLConnection httpURLConnection = (HttpsURLConnection) Url.openConnection();
 
-        final okhttp3.Request.Builder requestBuilder = new okhttp3.Request.Builder()
-                .method(httpMethod, requestBody).url(url)
-                .addHeader("User-Agent", USER_AGENT);
+        httpURLConnection.setRequestMethod(httpMethod);
+        httpURLConnection.setRequestProperty("User-Agent", USER_AGENT);
+        httpURLConnection.setUseCaches(false);
+        httpURLConnection.setDoInput(true);
+        if (dataToSend != null)
+            httpURLConnection.setDoOutput(true);
 
         for (Map.Entry<String, List<String>> pair : headers.entrySet()) {
             final String headerName = pair.getKey();
             final List<String> headerValueList = pair.getValue();
 
             if (headerValueList.size() > 1) {
-                requestBuilder.removeHeader(headerName);
+                boolean first = true;
                 for (String headerValue : headerValueList) {
-                    requestBuilder.addHeader(headerName, headerValue);
+                    if (first) {
+                        httpURLConnection.setRequestProperty(headerName, headerValue);
+                        first = false;
+                    }
+                    httpURLConnection.addRequestProperty(headerName, headerValue);
                 }
+
             } else if (headerValueList.size() == 1) {
-                requestBuilder.header(headerName, headerValueList.get(0));
+                httpURLConnection.setRequestProperty(headerName, headerValueList.get(0));
             }
         }
 
-        final okhttp3.Response response = client.newCall(requestBuilder.build()).execute();
+        if (dataToSend != null) {
+            OutputStream outStream = httpURLConnection.getOutputStream();
+            outStream.write(dataToSend);
+            outStream.flush();
+            outStream.close();
+        }
 
-        if (response.code() == 429) {
-            response.close();
-
+        if (httpURLConnection.getResponseCode() == 429)
             throw new ReCaptchaException("reCaptcha Challenge requested", url);
-        }
 
-        final ResponseBody body = response.body();
-        String responseBodyToReturn = null;
+        BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+        String inputLine;
 
-        if (body != null) {
-            responseBodyToReturn = body.string();
-        }
+        StringBuffer responseBody = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            responseBody.append(inputLine);
+        } in.close();
 
-        final String latestUrl = response.request().url().toString();
-        return new Response(response.code(), response.message(), response.headers().toMultimap(),
-                responseBodyToReturn, latestUrl);
+        return new Response(
+            httpURLConnection.getResponseCode(),
+            httpURLConnection.getResponseMessage(),
+            httpURLConnection.getHeaderFields(),
+            responseBody.toString(),
+            httpURLConnection.getURL().toString()
+        );
     }
 }
