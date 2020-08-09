875a0d2da6480724b52d007f634a05a0b0f02675
Kevin Pfeifer <kevin.pfeifer@kvnp.eu>
2020-08-10 01:55:43 +0200

    add rn-sliding-up-panel to playview

--- react/components/player/SwipePlaylist.js
+++ react/components/player/SwipePlaylist.js
@@ -1,111 +1,128 @@
-import React, { Component } from "react";
-import { Image, View, Text, StyleSheet, Pressable, Animated, Dimensions } from "react-native";
+import React, { PureComponent } from "react";
+import {
+    Image,
+    View,
+    Text,
+    StyleSheet,
+    Pressable,
+    Dimensions,
+    FlatList,
+    Animated
+} from "react-native";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import SlidingUpPanel from 'rn-sliding-up-panel';
+
 import { skipTo } from "../../service";
+import { appColor } from "../../styles/App";
 
-export default class SwipePlaylist extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isMinimized: true,
-            scrollAnim: new Animated.Value(this.props.minimumHeight)
-        };
-    }
-
-    scrollUp = () => {
-        if (this.state.isMinimized) {
-            this.setState({isMinimized: false});
-            Animated.timing(this.state.scrollAnim, {
-                toValue: Dimensions.get('window').height - 50,
-                duration: 300,
-                useNativeDriver: false
-            }).start();
-        }
-        
+const { height } = Dimensions.get("window");
+export default class SwipePlaylist extends PureComponent {
+    static defaultProps = {
+        draggableRange: { top: height-50, bottom: 50}
     };
-    
-    scrollDown = () => {
-        if (!this.state.isMinimized) {
-            this.setState({isMinimized: true});
-            Animated.timing(this.state.scrollAnim, {
-                toValue: this.props.minimumHeight,
-                duration: 300,
-                useNativeDriver: false
-            }).start();
-        }
-    };
-
-
+  
+    _draggedValue = new Animated.Value(50);
+  
     render() {
-        const flatlist = <Animated.FlatList
-                            style={[{height: this.state.scrollAnim}, this.props.style]}
-                            scrollEventThrottle={16}
-                            showsVerticalScrollIndicator={false}
-                            scrollEnabled={!this.state.isMinimized}
+        return (
+            <SlidingUpPanel
+                ref={(c) => (this._panel = c)}
+                draggableRange={this.props.draggableRange}
+                animatedValue={this._draggedValue}
+                snappingPoints={[50]}
+                height={height}
+                friction={0.5}
+            >
+                <View style={styles.panel}>
+                    <Pressable style={styles.panelHeader} onPress={() => this._panel.show()}>
+                        <View style={stylesRest.smallBar}/>
+                        <Text style={{color: "white"}}>WIEDERGABELISTE</Text>
+                    </Pressable>
 
-                            onScroll={event => {
-                                const currentOffset = event.nativeEvent.contentOffset.y;
-                                const dif = currentOffset - (this.offset || 0);
+                    <FlatList
+                        style={{backgroundColor: appColor.background.backgroundColor, height: height - 150}}
+                        contentContainerStyle={stylesRest.playlistContainer}
 
-                                if (dif < 0 && currentOffset < 10) {
-                                    this.scrollDown();
-                                    //scrollToOffset({animated: true, offset: 0});
+                        data={this.props.playlist}
+
+                        /*ListFooterComponentStyle={this.state.isMinimized ?{display: "none"} :stylesRest.topAlign}
+                        ListFooterComponent={
+                            <Pressable onPress={this.scrollDown}>
+                                <View style={stylesRest.smallBar}/>
+                            </Pressable>
+                        }*/
+
+                        keyExtractor={item => item.id}
+                        renderItem={({item, index}) => 
+                            <Pressable style={{
+                                            height: 50,
+                                            flexDirection: "row",
+                                            alignItems: "center",
+                                            marginVertical: 5
+                                        }}
+
+                                        onPress={() => skipTo(item.id)}
+                            >
+                                {
+                                    this.props.track.id == item.id
+                                        ? <MaterialIcons style={{width: 30, textAlign: "center", textAlignVertical: "center"}} name="play-arrow" color="white" size={20}/>
+                                        : <Text style={{width: 30, textAlign: "center", fontSize: 15, color: "white"}}>{index + 1}</Text>
                                 }
-                            }}
 
-                            onScrollToTop={this.scrollDown}
+                                <Image style={{height: 50, width: 50, marginRight: 10}} source={{uri: item.artwork}}/>
 
-                            data={this.props.playlist}
-
-                            ListHeaderComponentStyle={stylesRest.topAlign}
-                            ListHeaderComponent={
-                                <Pressable onPress={this.scrollUp}>
-                                    <View style={stylesRest.smallBar}/>
-                                    <Text style={{color: "white"}}>WIEDERGABELISTE</Text>
-                                </Pressable>
-                            }
-
-                            ListFooterComponentStyle={this.state.isMinimized ?{display: "none"} :stylesRest.topAlign}
-                            ListFooterComponent={
-                                <Pressable onPress={this.scrollDown}>
-                                    <View style={stylesRest.smallBar}/>
-                                </Pressable>
-                            }
-
-                            keyExtractor={item => item.id}
-                            contentContainerStyle={{width: "100%", paddingHorizontal: 10}}
-                            renderItem={({item, index}) => 
-                                <Pressable style={{
-                                                height: 50,
-                                                flexDirection: "row",
-                                                alignItems: "center",
-                                                marginVertical: 5
-                                            }}
-
-                                            onPress={() => skipTo(item.id)}
-                                >
-                                    {
-                                        this.props.track.id == item.id
-                                            ? <MaterialIcons style={{width: 30, textAlign: "center", textAlignVertical: "center"}} name="play-arrow" color="white" size={20}/>
-                                            : <Text style={{width: 30, textAlign: "center", fontSize: 15, color: "white"}}>{index + 1}</Text>
-                                    }
-
-                                    <Image style={{height: 50, width: 50, marginRight: 10}} source={{uri: item.artwork}}/>
-
-                                    <View style={{width: 0, flexGrow: 1, flex: 1}}>
-                                        <Text style={{color: "white"}}>{item.title}</Text>
-                                        <Text style={{color: "white"}}>{item.artist}</Text>
-                                    </View>
-                                </Pressable>
-                            }/>
-
-        return flatlist;
+                                <View style={{width: 0, flexGrow: 1, flex: 1}}>
+                                    <Text style={{color: "white"}}>{item.title}</Text>
+                                    <Text style={{color: "white"}}>{item.artist}</Text>
+                                </View>
+                            </Pressable>
+                        }
+                    />
+                    
+                </View>
+            </SlidingUpPanel>
+        );
     }
 }
 
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "stretch",
+        justifyContent: "center",
+        backgroundColor: appColor.background.backgroundColor,
+        
+    },
+
+    panel: {
+        flex: 1,
+        backgroundColor: "transparent",
+        position: "relative"
+    },
+
+    panelHeader: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: 50,
+        backgroundColor: appColor.background.backgroundColor,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+    },
+
+    textHeader: {
+        fontSize: 28,
+        color: "#FFF"
+    }
+});
+
 const stylesRest = StyleSheet.create({
-    playlistContainer: {height: 50, flexDirection: "row", width: "100%"},
+    playlistContainer: {
+        width: "100%",
+        backgroundColor: appColor.background.backgroundColor,
+        paddingHorizontal: 10,
+        paddingBottom: 50
+    },
 
     topAlign: {
         alignSelf: "center",
