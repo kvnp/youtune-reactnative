69dbdbe70a913deba765a30e99a4e217c3c9abeb
Kevin Pfeifer <kevin.pfeifer@kvnp.eu>
2020-08-10 00:27:47 +0200

    setupPlayer() has no use

--- react/service.js
+++ react/service.js
@@ -14,7 +14,6 @@ export const register = () => {
 
     TrackPlayer.registerPlaybackService(() => require("./handler"));
 
-    TrackPlayer.setupPlayer();
     TrackPlayer.updateOptions({
         stopWithApp: true,
         alwaysPauseOnInterruption: true,
