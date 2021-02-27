67f61829b1a5a9e3dcca739de18c1855df7d6db0
kevinpfeifer <kevin.pfeifer8@gmail.com>
2021-02-27 20:34:40 +0100

    store songs with indexeddb on web

--- react/modules/storage/SongStorage.js
+++ react/modules/storage/SongStorage.js
@@ -4,14 +4,47 @@ import { downloadMedia, fetchAudioStream, fetchVideoInfo } from "../remote/API";
 
 export var localIDs = null;
 
-async function loadSongList() {
-    const keys = await AsyncStorage.getAllKeys();
+if (Platform.OS == "web")
+    var db = null; 
 
+async function loadSongList(db) {
     localIDs = [];
-    if (keys.length > 0) {
-        for (let i = 0; i < keys.length; i++) {
-            if (keys[i].includes('@storage_Song_')) {
-                localIDs.push(keys[i].substring(14));
+
+    if (Platform.OS == "web") {
+        var request = indexedDB.open("storage", 1);
+        request.onupgradeneeded = event => {
+            let db = request.result;
+            if (!db.objectStoreNames.contains('songs'))
+                db.createObjectStore('songs', {keyPath: 'id'});
+
+            console.log("DB upgraded");
+        };
+
+        request.onerror = event => {
+            console.log("failed opening DB: " + request.errorCode);
+        };
+
+        request.onsuccess = event => {
+            console.log("opened DB");
+            db = request.result;
+
+            request = db.transaction("songs", "readonly")
+                .objectStore("songs")
+                .getAllKeys();
+
+            request.onsuccess = event => {
+                localIDs = request.result;
+                console.log(localIDs);
+            };
+        };
+
+    } else {
+        const keys = await AsyncStorage.getAllKeys();
+        if (keys.length > 0) {
+            for (let i = 0; i < keys.length; i++) {
+                if (keys[i].includes('@storage_Song_')) {
+                    localIDs.push(keys[i].substring(14));
+                }
             }
         }
     }
@@ -20,33 +53,87 @@ async function loadSongList() {
 loadSongList();
 
 export async function loadSongLocal(id) {
-    try {
-        const value = await AsyncStorage.getItem('@storage_Song_' + id);
+    if (Platform.OS == "web") {
+        return new Promise((resolve, reject) => {
+            var request = indexedDB.open("storage");
 
-        if (value !== null)
-            return JSON.parse(value);
-        else
+            request.onerror = event => {
+                console.log("failed opening DB: " + request.errorCode);
+                reject("failed opening DB: " + request.errorCode);
+            };
+
+            request.onsuccess = event => {
+                console.log("opened DB");
+                db = request.result;
+
+                request = db.transaction("songs", "readonly")
+                    .objectStore("songs")
+                    .get(id);
+
+                request.onsuccess = event => {
+                    let track = request.result.track;
+                    resolve(track);
+                };
+                
+            };
+        });
+
+    } else {
+        try {
+            const value = await AsyncStorage.getItem('@storage_Song_' + id);
+
+            if (value !== null)
+                return JSON.parse(value);
+            else
+                return null;
+        } catch(e) {
+            alert("Loading sond failed - " + id);
             return null;
-    } catch(e) {
-        alert("Loading sond failed - " + id);
-        return null;
+        }
     }
 }
 
-export async function storeSong(id, pictureBlob, audioBlob) {
-    try {
-        const string = JSON.stringify({
-            id: id,
-            picture: await pictureBlob.text(),
-            audio: await audioBlob.text()
+export async function storeSong(id, track) {
+    if (Platform.OS == "web") {
+        return new Promise((resolve, reject) => {
+            var request = indexedDB.open("storage");
+
+            request.onerror = event => {
+                console.log("failed opening DB: " + request.errorCode);
+                reject("failed opening DB: " + request.errorCode);
+            };
+
+            request.onsuccess = event => {
+                console.log("opened DB");
+                db = request.result;
+
+                db.transaction("songs", "readwrite")
+                    .objectStore("songs")
+                    .add({
+                        id: id,
+                        track: track
+                    });
+
+                resolve(track);
+            };
         });
 
-        await AsyncStorage.setItem('@storage_Song_' + id, string);
-        loadSongList();
-    } catch(e) {
-        console.log(e);
-        alert("Storing song failed - " + id);
-        return null;
+        return 
+    } else {
+        try {
+            const string = JSON.stringify({
+                id: id,
+                picture: await pictureBlob.text(),
+                audio: await audioBlob.text()
+            });
+    
+            await AsyncStorage.setItem('@storage_Song_' + id, string);
+            loadSongList();
+        } catch(e) {
+            console.log(e);
+            alert("Storing song failed - " + id);
+            return null;
+        }
     }
 }
 
@@ -54,22 +141,8 @@ export const downloadSong = async(id) => {
     let track = await fetchVideoInfo(id);
     track.url = await fetchAudioStream(id);
 
-    let targetHeader;
-    let thumbnailUrl;
-    let audioUrl;
+    track.artwork = await downloadMedia(track.artwork, targetHeader);
+    track.url = await downloadMedia(track.url, targetHeader);
 
-    if (Platform.OS == "web") {
-        targetHeader = track.url.split("/")[2];
-        thumbnailUrl = document.location.href + track.thumbnail.split("/").slice(3).join("/");
-        audioUrl = document.location.href + "video/" + track.url.split("/").slice(3).join("/");
-    } else {
-        thumbnailUrl = track.thumbnail;
-        audioUrl = track.url;
-        targetHeader = null;
-    }
-
-    let pictureBlob = await downloadMedia(thumbnailUrl, targetHeader);
-    let audioBlob = await downloadMedia(audioUrl, targetHeader);
-    console.log(audioBlob);
-    storeSong(id, pictureBlob, audioBlob);
+    storeSong(id, track);
 }
