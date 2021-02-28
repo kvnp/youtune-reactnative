042334dbd8748875ace10b5c59bca41e2a2005f9
kevinpfeifer <kevin.pfeifer8@gmail.com>
2021-02-28 02:28:43 +0100

    replace deprecated community async storage

--- react/modules/storage/SongStorage.js
+++ react/modules/storage/SongStorage.js
@@ -1,4 +1,4 @@
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
 import { downloadCallback } from "../../components/shared/MoreModal";
 import { downloadMedia, fetchAudioStream, fetchVideoInfo } from "../remote/API";
