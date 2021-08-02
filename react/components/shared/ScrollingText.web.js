67786bad7e3e3f14f414ce7311adf1e1cfa2ea9d
kevinpfeifer <kevin.pfeifer8@gmail.com>
2021-08-02 04:05:39 +0200

    manipulate css directly on web

---
+++ react/components/shared/ScrollingText.web.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useRef } from "react";
+import { ScrollView, View } from "react-native";
+import { unstable_createElement } from "react-native-web";
+
+const ScrollingText = ({children, style}) => {
+    const [containerWidth, setContainerWidth] = useState(-1);
+    const [contentWidth, setContentWidth] = useState(-1);
+
+    const Style = props => unstable_createElement("style", props);
+    const viewRef = useRef(null);
+
+    useEffect(() => {
+        viewRef.current.style.animationTimingFunction = "linear";
+        viewRef.current.style.animationDuration = "10s";
+        viewRef.current.style.transformOrigin = "0%";
+    }, []);
+
+    useEffect(() => {
+        if (isOverflowing())
+            startAnimation();
+        else
+            stopAnimation();
+    }, [contentWidth, containerWidth]);
+
+    const startAnimation = () => {
+        viewRef.current.style.animationDelay = "2s";
+        viewRef.current.style.animationIterationCount = "1";
+        viewRef.current.style.animationName = children.props.children.replaceAll(" ", "_").replace(/[\W_]+/g,"-");
+        viewRef.current.onanimationend = e => {
+            viewRef.current.style.animationDelay = "0s";
+            viewRef.current.style.animationIterationCount = "infinite";
+            viewRef.current.style.animationName = children.props.children.replaceAll(" ", "_").replace(/[\W_]+/g,"-") + "_2";
+        }
+    }
+
+    const stopAnimation = () => {
+        viewRef.current.style.animationName = "";
+    }
+
+    const areWidthsNotDefined = () => {
+        return (contentWidth == -1 || containerWidth == -1);
+    }
+
+    const isOverflowing = () => {
+        return areWidthsNotDefined()
+            ? false
+            : contentWidth > containerWidth;
+    }
+
+    return <View
+        style={[{overflow: "hidden", width: "100%"}, style]}
+        onLayout={
+            e => setContainerWidth(e.nativeEvent.layout.width)
+        }
+    >
+        <Style>
+            {
+                children.props.children
+                    ? `@keyframes ${children.props.children.replaceAll(" ", "_").replace(/[\W_]+/g,"-")} { from {transform: translateX(0%)} to {transform: translateX(-100%)}}\n` +
+                    `@keyframes ${children.props.children.replaceAll(" ", "_").replace(/[\W_]+/g,"-")}_2 { from {transform: translateX(${containerWidth/contentWidth*100}%)} to {transform: translateX(-100%)}}`
+                    : null
+            }
+        </Style>
+        <ScrollView
+            ref={viewRef}
+            onContentSizeChange={
+                width => setContentWidth(width)
+            }
+
+            style={{
+                alignSelf: isOverflowing()
+                    ? "flex-start"
+                    : "center",
+            }}
+        >
+            <View
+                style={{
+                    alignSelf: "center",
+                    justifyContent: "center",
+                    width: "auto"
+                }}
+            >
+                {children}
+            </View>
+        </ScrollView>
+    </View>;
+}
+
+export default ScrollingText;
