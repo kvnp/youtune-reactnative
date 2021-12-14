74de94baf1a8c11a412a27ba73fa232c880b333b
Kevin Pfeifer <kevin1174@schule.bremen.de>
2021-12-14 23:08:19 +0100

    make use of blobs

--- react/services/device/IO.js
+++ react/services/device/IO.js
@@ -1,9 +1,36 @@
 import Media from "../api/Media";
 
 export default class IO {
-    static readBase64({blob, url}) {
+    static isBlob(data) {
+        return data instanceof Blob;
+    }
+
+    static isBase64(base64) {
+        try {
+            if(btoa(atob(base64)) == base64)
+                return true;
+        } catch (_) {}
+
+        return false;
+    }
+
+    static isURL(url) {
+        try {
+            url = new URL(url);
+            return true;
+        } catch (_) {
+            return false;
+        }
+    }
+
+    static getBlobAsBase64({blob, url, controllerCallback}) {
         return new Promise(async(resolve, reject) => {
-            if (url) blob = await Media.getBlob(url);
+            if (url) {
+                if (this.isBlob(url))
+                    blob = url;
+                else
+                    blob = await Media.getBlob({url, controllerCallback});
+            }
 
             let fileReader = new FileReader();
             fileReader.readAsDataURL(blob);
@@ -12,5 +39,9 @@ export default class IO {
                 resolve(fileReader.result);
             }
         })
+    }
+
+    static getBlobAsURL(blob) {
+        return URL.createObjectURL(blob);
     }
 }
