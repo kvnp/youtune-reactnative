6fa0b54d59514e08eb599334af07430c609e2c34
Kevin Pfeifer <kevin.pfeifer8@gmail.com>
2022-05-22 17:46:31 +0200

    add transition to seekbar

--- react/components/player/SeekBar.web.js
+++ react/components/player/SeekBar.web.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 
 import { useTheme } from '@react-navigation/native';
@@ -17,7 +17,7 @@ const minutesAndSeconds = position => ([
     pad( ~~(position % 60), 2),
 ]);
 
-export default SeekBar = ({style, buffering}) => {
+export default SeekBar = ({style}) => {
     const { colors } = useTheme();
     const { duration } = Music.metadata;
     const [width, setWidth] = useState("99.99%");
@@ -33,6 +33,43 @@ export default SeekBar = ({style, buffering}) => {
 
     const elapsed = minutesAndSeconds(realPosition);
     const remaining = minutesAndSeconds(duration - realPosition);
+    const container = useRef(null);
+
+    useEffect(() => {
+        let slider = container.current.childNodes[0];
+        let leftBar = slider.childNodes[0];
+        let rightBar = slider.childNodes[2];
+        leftBar.style.height = "5px";
+        rightBar.style.height = "5px";
+
+        leftBar.style.borderRadius = "";
+        leftBar.style.borderBottomLeftRadius = "5px";
+        leftBar.style.borderTopLeftRadius = "5px";
+        rightBar.style.borderRadius = "";
+        rightBar.style.borderBottomRightRadius = "5px";
+        rightBar.style.borderTopRightRadius = "5px";
+        leftBar.style.transition = "height .3s";
+        rightBar.style.transition = "height .3s";
+        let thumb = slider.childNodes[1];
+        thumb.style.transition = "width .3s";
+
+        const enter = () => {
+            thumb.style.width = "0px";
+            leftBar.style.height = "15px";
+            rightBar.style.height = "15px";
+        };
+    
+        const leave = () => {
+            thumb.style.width = "20px";
+            leftBar.style.height = "5px";
+            rightBar.style.height = "5px";
+        };
+
+        container.current.onmouseenter = () => enter();
+        container.current.ontouchstart = () => enter();
+        container.current.onmouseleave = () => leave();
+        container.current.ontouchend = () => leave();
+    }, []);
 
     useEffect(() => {
         setWidth("100%");
@@ -44,7 +81,7 @@ export default SeekBar = ({style, buffering}) => {
         return () => listener.remove();
     }, []);
 
-    return <View style={[styles.container, style]}>
+    return <View ref={container} style={[styles.container, style]}>
         <Slider
             onSlidingComplete={position => {
                 Music.seekTo(position);
@@ -76,7 +113,6 @@ export default SeekBar = ({style, buffering}) => {
             maximumTrackTintColor={colors.card}
             thumbTintColor={colors.primary}
             thumbStyle={{color: colors.primary}}
-            trackStyle={styles.track}
             style={{width: width}}
         />
 
@@ -102,7 +138,6 @@ const styles = StyleSheet.create({
     },
 
     track: {
-        height: 2,
         borderRadius: 1
     },
 
