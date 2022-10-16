0e196ab3529fa2f3319bfd22748e4a3f435fa1e9
Kevin Pfeifer <kevin.pfeifer8@gmail.com>
2022-10-16 20:48:55 +0200

    remove fontconfig

--- react/Config.ts
+++ react/Config.ts
@@ -1,101 +1,5 @@
-import { configureFonts, DefaultTheme as PaperTheme} from 'react-native-paper';
+import { DefaultTheme as PaperTheme} from 'react-native-paper';
 import { DefaultTheme, DarkTheme } from '@react-navigation/native';
-
-const fontConfig = {
-    //-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif
-    default: {
-        regular: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-        medium: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-        light: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-        thin: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-    },
-    ios: {
-        regular: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-        medium: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-        light: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-        thin: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-    },
-    android: {
-        regular: {
-            fontFamily: 'Roboto-Regular',
-            fontWeight: 'normal',
-        },
-        medium: {
-            fontFamily: 'Roboto-Medium',
-            fontWeight: 'normal',
-        },
-        light: {
-            fontFamily: 'Roboto-Light',
-            fontWeight: 'normal',
-        },
-        thin: {
-            fontFamily: 'Roboto-Thin',
-            fontWeight: 'normal',
-        },
-        black: {
-            fontFamily: 'Roboto-Black',
-            fontWeight: 'normal',
-        },
-        bold: {
-            fontFamily: 'Roboto-Bold',
-            fontWeight: 'normal',
-        },
-    },
-    macos: {
-        regular: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-        medium: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-        light: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-        thin: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-    },
-};
 
 export const linking = {
     enabled: true,
@@ -128,7 +32,7 @@ export const linking = {
     }
 };
 
-export const getTheme = isDark => {
+export const getTheme = (isDark: boolean) => {
     return isDark
         ? {
             ...DarkTheme,
@@ -137,8 +41,7 @@ export const getTheme = isDark => {
                 ...DarkTheme.colors,
                 onSurface: DarkTheme.colors.border
             },
-            dark: true,
-            fonts: configureFonts(fontConfig)
+            dark: true
         }
 
         : {
@@ -148,7 +51,6 @@ export const getTheme = isDark => {
                 ...DefaultTheme.colors,
                 onSurface: DarkTheme.colors.border
             },
-            dark: false,
-            fonts: configureFonts(fontConfig)
+            dark: false
         }
 }
