d801071d2dc0aa7d37f2da198c0d6a557e2313a4
Kevin Pfeifer <kevin1174@schule.bremen.de>
2022-10-19 11:55:19 +0200

    remove font config

--- react/Config.js
+++ react/Config.js
@@ -1,102 +1,6 @@
 import { configureFonts, DefaultTheme as PaperTheme} from 'react-native-paper';
 import { DefaultTheme, DarkTheme } from '@react-navigation/native';
 
-const fontConfig = {
-    //-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif
-    default: {
-        regular: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-        medium: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-        light: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-        thin: {
-            fontFamily: 'Segoe UI',
-            fontWeight: 'normal',
-            textTransform: "none",
-            letterSpacing: 0,
-            fontSize: 14
-        },
-    },
-    ios: {
-        regular: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-        medium: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-        light: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-        thin: {
-            fontFamily: '-apple-system',
-            fontWeight: 'normal',
-        },
-    },
-    android: {
-        regular: {
-            fontFamily: 'Roboto-Regular',
-            fontWeight: 'normal',
-        },
-        medium: {
-            fontFamily: 'Roboto-Medium',
-            fontWeight: 'normal',
-        },
-        light: {
-            fontFamily: 'Roboto-Light',
-            fontWeight: 'normal',
-        },
-        thin: {
-            fontFamily: 'Roboto-Thin',
-            fontWeight: 'normal',
-        },
-        black: {
-            fontFamily: 'Roboto-Black',
-            fontWeight: 'normal',
-        },
-        bold: {
-            fontFamily: 'Roboto-Bold',
-            fontWeight: 'normal',
-        },
-    },
-    macos: {
-        regular: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-        medium: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-        light: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-        thin: {
-            fontFamily: 'BlinkMacSystemFont',
-            fontWeight: 'normal',
-        },
-    },
-};
-
 export const linking = {
     enabled: true,
     prefixes: ["https://youtune.kvnp.eu"],
@@ -129,26 +33,13 @@ export const linking = {
 };
 
 export const getTheme = isDark => {
-    return isDark
-        ? {
-            ...DarkTheme,
-            ...PaperTheme,
-            colors: {
-                ...DarkTheme.colors,
-                onSurface: DarkTheme.colors.border
-            },
-            dark: true,
-            fonts: configureFonts(fontConfig)
-        }
-
-        : {
-            ...DefaultTheme,
-            ...PaperTheme,
-            colors: {
-                ...DefaultTheme.colors,
-                onSurface: DarkTheme.colors.border
-            },
-            dark: false,
-            fonts: configureFonts(fontConfig)
-        }
+    return {
+        ...DarkTheme,
+        ...PaperTheme,
+        colors: {
+            ...DarkTheme.colors,
+            onSurface: DarkTheme.colors.border
+        },
+        dark: isDark,
+    };
 }
