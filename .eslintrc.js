91e08b521d53e050c299591b0f78b03d25ae5259
Kevin Pfeifer <kevin.pfeifer8@gmail.com>
2023-02-19 02:08:26 +0100

    update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ]
 };
